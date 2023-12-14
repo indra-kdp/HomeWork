df072e0a141c90747364d7526b56c130012ba0b6
indra-kdp <149049824+indra-kdp@users.noreply.github.com>
2023-12-15 00:16:11 +0300

    Add files via upload

---
+++ 7-1/script.ts
@@ -0,0 +1,24 @@
+interface Enot {
+    name: string;
+    year: number;
+    weight: number;
+    image?: string;
+}
+
+function Generation() {
+    const name = (document.getElementById('ENOT') as HTMLInputElement).value;
+    const year = Math.floor(Math.random() * 10);
+    const weight = Math.floor(Math.random() * 10);
+
+    let enot: Enot = {
+        name: name,
+        weight: weight,
+        year: year,
+    }
+
+    let infoEnot = document.getElementById('card') as HTMLDivElement;
+    infoEnot.innerHTML = `Имя: ${enot.name}; Возраст: ${enot.year}; Вес: ${enot.weight}кг`;
+    
+    let enotikArray = [];
+    enotikArray.push(enot);
+}
